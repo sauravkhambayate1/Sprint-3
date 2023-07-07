27f8f79ad78a57dd8a4f541a0b57a80b9e4eb568
Satya <satyanaidugandrothu@gmail.com>
2023-07-07 12:18:11 +0530

    sprint 3 pushed

---
+++ src/main/java/com/satya/ItemsAPI/repository/ItemRepository.java
@@ -0,0 +1,11 @@
+package com.satya.ItemsAPI.repository;
+
+import com.satya.ItemsAPI.repository.entity.Item;
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+public interface ItemRepository
+    extends CrudRepository<Item, Integer>
+{
+}
---
+++ src/main/java/com/satya/ItemsAPI/service/ItemService.java
@@ -0,0 +1,18 @@
+package com.satya.ItemsAPI.service;
+
+import com.satya.ItemsAPI.repository.entity.Item;
+
+import java.util.List;
+
+public interface ItemService
+{
+
+    Item save( Item item );
+
+    void delete( int itemId );
+
+    List<Item> all();
+
+    Item findById( int itemId );
+
+}
